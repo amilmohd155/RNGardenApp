1209e32bf3306179bc8bb73f0c22ee17504d6d86
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2024-04-06 22:07:42 +0100

    Update theme selection styles in theme.tsx

--- app/(settings)/theme.tsx
+++ app/(settings)/theme.tsx
@@ -41,19 +41,19 @@ const ThemeSelectionScreen = () => {
       >
         <Text className="text-4xl font-bold">Select your theme</Text>
         {/* Dark */}
-        <Pressable className="my-3 flex-row items-center justify-around gap-5 rounded-2xl border-2 border-[#1d1f25] bg-[#3b3f4f] p-5">
+        <Pressable className=" my-3 flex-row items-center justify-around gap-5 rounded-2xl border-2 border-[#1d1f25] bg-[#3b3f4f] p-5 active:bg-[#1d1f25]">
           <Ionicons name="moon-outline" size={32} color="#eff3ec" />
           <Text className="text-4xl font-semibold text-surface">Dark</Text>
           <Ionicons name="checkmark" size={32} color="#eff3ec" />
         </Pressable>
         {/* Light */}
-        <Pressable className="my-3 flex-row items-center justify-around rounded-2xl border-2 border-green-400 bg-[#86aaa7] p-5">
+        <Pressable className="my-3 flex-row items-center justify-around rounded-2xl border-2 border-green-400 bg-[#86aaa7] p-5 active:bg-green-400">
           <Ionicons name="sunny-outline" size={32} color="#374544" />
           <Text className="text-4xl font-semibold text-[#374544]">Light</Text>
           <Ionicons name="checkmark" size={32} color="#374544" />
         </Pressable>
         {/* Auto */}
-        <Pressable className="my-3 flex-row items-center justify-around rounded-2xl border-2 border-gray-600 bg-gray-50 p-5">
+        <Pressable className="my-3 flex-row items-center justify-around rounded-2xl border-2 border-gray-600 bg-gray-50 p-5 active:bg-gray-400">
           <Ionicons name="cog-outline" size={32} color="rgb(55 65 81)" />
           <Text className="text-4xl font-semibold text-gray-700">Auto</Text>
           <Ionicons name="checkmark" size={32} color="rgb(55 65 81)" />
