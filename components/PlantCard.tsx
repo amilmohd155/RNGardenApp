eb5498432ca4888656469e872486202a592d47fb
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2024-04-16 21:30:11 +0100

    Refactor RotatingBorderIcon component, update dependencies, and fix styling issues

--- components/PlantCard.tsx
+++ components/PlantCard.tsx
@@ -15,7 +15,7 @@ import {
 import { Image } from "expo-image";
 import { Link } from "expo-router";
 import { useMemo, useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { LayoutChangeEvent, Pressable, Text, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   Easing,
@@ -47,6 +47,7 @@ export const PlantCard = ({
   portion: quantity,
 }: PlantCardProps) => {
   const [watered, setWatered] = useState(false);
+  const [layoutHeight, setLayoutHeight] = useState(0);
 
   const translateX = useSharedValue(0);
 
@@ -75,7 +76,7 @@ export const PlantCard = ({
       // } else {
       //   translateX.value = withTiming(0);
       // }
-      translateX.value = withTiming(0);
+      // translateX.value = withTiming(0);
     });
 
   const panStyle = useAnimatedStyle(() => {
@@ -88,89 +89,103 @@ export const PlantCard = ({
     };
   });
 
+  function onLayout(event: LayoutChangeEvent): void {
+    const { height } = event.nativeEvent.layout;
+    setLayoutHeight(height);
+  }
+
   return (
-    <RotatingBorderIcon />
+    <Link
+      href={{
+        pathname: "/(plant)/[id]",
+        params: { id },
+      }}
+      asChild
+    >
+      <Pressable className="justify-center" onLayout={onLayout}>
+        <RotatingBorderIcon
+          translateX={translateX}
+          layoutHeight={layoutHeight}
+          svg={require("@/assets/icons/trash.svg")}
+        />
+        <RotatingBorderIcon
+          translateX={translateX}
+          layoutHeight={layoutHeight}
+          svg={require("@/assets/icons/watering.svg")}
+          right
+        />
+        {/* <MaterialCommunityIcons
+          name="trash-can"
+          size={50}
+          className="{}-[color]: absolute left-10 rounded-full border-4 bg-green-950 p-5 text-center color-black"
+        />
+        <MaterialCommunityIcons
+          name="watering-can"
+          size={50}
+          className="{}-[color]: absolute right-10 rounded-full border-4 bg-green-950 p-5 text-center color-black"
+        /> */}
+        <GestureDetector gesture={panGesture}>
+          <Animated.View
+            style={[panStyle]}
+            className="flex-row justify-between rounded-xl bg-secondary"
+          >
+            <View className="justify-between p-5">
+              <View className="gap-1">
+                {/* Scientific Name */}
+                {scientificName && (
+                  <Text className="text-sm font-bold italic text-onTertiary/50">
+                    {scientificName}
+                  </Text>
+                )}
 
-    // <Link
-    //   href={{
-    //     pathname: "/(plant)/[id]",
-    //     params: { id },
-    //   }}
-    //   asChild
-    // >
-    //   <Pressable className="justify-center">
-    //     {/* <MaterialCommunityIcons
-    //       name="trash-can"
-    //       size={50}
-    //       className="{}-[color]: absolute left-10 rounded-full border-4 bg-green-950 p-5 text-center color-black"
-    //     />
-    //     <MaterialCommunityIcons
-    //       name="watering-can"
-    //       size={50}
-    //       className="{}-[color]: absolute right-10 rounded-full border-4 bg-green-950 p-5 text-center color-black"
-    //     /> */}
-    //     <GestureDetector gesture={panGesture}>
-    //       <Animated.View
-    //         style={[panStyle]}
-    //         className="bg-secondary flex-row justify-between rounded-xl"
-    //       >
-    //         <View className="justify-between p-5">
-    //           <View className="gap-1">
-    //             {/* Scientific Name */}
-    //             {scientificName && (
-    //               <Text className="text-onTertiary/50 text-sm font-bold italic">
-    //                 {scientificName}
-    //               </Text>
-    //             )}
-
-    //             {/* Plant Name */}
-    //             <Text className="text-onSecondary text-xl font-bold">
-    //               {alias}
-    //             </Text>
-    //             {/* Location */}
-    //             <Text className="text-onSecondary/75 font-semibold">
-    //               {room}
-    //             </Text>
-    //           </View>
-    //           {/* Reminder */}
-    //           <View className="border-outline bg-tertiary shrink flex-row items-center gap-1 rounded-2xl border-2 px-3 py-2">
-    //             <Ionicons
-    //               name="water-outline"
-    //               size={24}
-    //               color={Colors.onTertiaryContainer}
-    //             />
-    //             <Text className="text-onTertiary text-lg font-bold">{`${quantity}ml in ${period} days`}</Text>
-    //           </View>
-    //         </View>
-    //         <Animated.View
-    //           sharedTransitionTag={`plant-image-${id}`}
-    //           className="h-40 w-40"
-    //         >
-    //           <Image
-    //             className="flex-1 rounded-br-xl rounded-tr-xl"
-    //             source={image}
-    //             placeholder={blurhash}
-    //             contentFit="cover"
-    //             transition={1000}
-    //           />
-    //         </Animated.View>
-    //         {/* Watered */}
-    //         <Pressable
-    //           onPress={() => setWatered(!watered)}
-    //           className="bg-primary/50 active:bg-tertiary absolute bottom-0 right-0 w-40 flex-row items-center gap-2 rounded-br-xl px-4 py-3"
-    //         >
-    //           <MaterialCommunityIcons
-    //             name="watering-can"
-    //             size={24}
-    //             className="{}-[color]: color-white"
-    //           />
-    //           <Text className="text-xl font-semibold text-white">
-    //             {watered ? "Watered" : "Not Watered"}
-    //           </Text>
-    //         </Pressable>
-    //       </Animated.View>
-    //     </GestureDetector>
-    //   </Pressable>
-    // </Link>
+                {/* Plant Name */}
+                <Text className="text-xl font-bold text-onSecondary">
+                  {alias}
+                </Text>
+                {/* Location */}
+                <Text className="font-semibold text-onSecondary/75">
+                  {room}
+                </Text>
+              </View>
+              {/* Reminder */}
+              <View className="shrink flex-row items-center gap-1 rounded-2xl border-2 border-outline bg-tertiary px-3 py-2">
+                <Ionicons
+                  name="water-outline"
+                  size={24}
+                  color={Colors.onTertiaryContainer}
+                />
+                <Text className="text-lg font-bold text-onTertiary">{`${quantity}ml in ${period} days`}</Text>
+              </View>
+            </View>
+            <Animated.View
+              sharedTransitionTag={`plant-image-${id}`}
+              className="h-40 w-40"
+            >
+              <Image
+                className="flex-1 rounded-br-xl rounded-tr-xl"
+                source={image}
+                placeholder={blurhash}
+                contentFit="cover"
+                transition={1000}
+              />
+            </Animated.View>
+            {/* Watered */}
+            <Pressable
+              onPress={() => setWatered(!watered)}
+              className="absolute bottom-0 right-0 w-40 flex-row items-center gap-2 rounded-br-xl bg-primary/50 px-4 py-3 active:bg-tertiary"
+            >
+              <MaterialCommunityIcons
+                name="watering-can"
+                size={24}
+                className="{}-[color]: color-white"
+              />
+              <Text className="text-xl font-semibold text-white">
+                {watered ? "Watered" : "Not Watered"}
+              </Text>
+            </Pressable>
+          </Animated.View>
+        </GestureDetector>
+      </Pressable>
+    </Link>
   );
 };
--- components/RotatingBorderIcon.tsx
+++ components/RotatingBorderIcon.tsx
@@ -1,61 +1,109 @@
+import { SCREEN_WIDTH } from "@gorhom/bottom-sheet";
 import {
-  useTiming,
   Skia,
   Paint,
   SweepGradient,
   vec,
-  size,
   Canvas,
   Path,
   Vector,
-  Circle,
+  useSVG,
+  ImageSVG,
+  Group,
+  DataSourceParam,
+  fitbox,
+  rect,
 } from "@shopify/react-native-skia";
 import { PropsWithChildren, useMemo } from "react";
-import { Easing } from "react-native";
+import { StyleProp, ViewStyle } from "react-native";
+import {
+  Easing,
+  SharedValue,
+  useAnimatedReaction,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+const TRANSLATE_X_THRESHOLD = SCREEN_WIDTH * 0.3;
+const MIN_TRANSLATE_X = SCREEN_WIDTH * 0.1;
+
+const clampValue = (value: number, threshold: number): number => {
+  "worklet";
+  const abs = Math.abs(value);
+  const normalized = abs / threshold;
+  const normalizedMinThreshold = MIN_TRANSLATE_X / threshold;
+
+  if (normalized < normalizedMinThreshold) return 0;
+  return Math.min(normalized - normalizedMinThreshold, 1);
+};
 
 const fromCircle = (center: Vector, r: number) => {
   "worklet";
-  return Skia.XYWHRect(center.x - r, center.y - r, r * 2, r * 2);
+  return Skia.XYWHRect(center.x, center.y, r, r);
 };
 
-type Props = {};
+type Props = {
+  translateX: SharedValue<number>;
+  layoutHeight?: number;
+  svg?: DataSourceParam;
+  right?: boolean;
+};
 
-export const RotatingBorderIcon = (props: PropsWithChildren<Props>) => {
-  const size = 150;
+export const RotatingBorderIcon = ({
+  layoutHeight: size = 140,
+  translateX,
+  svg: inputSVG = require("@/assets/icons/trash.svg"),
+
+  right = false,
+  children,
+}: PropsWithChildren<Props>) => {
+  const percentage = useSharedValue(0);
+
+  const x = size / 4;
+  const y = size / 4;
   const radius = size / 2;
   const strokeWidth = radius * 0.05;
-  const progress = useTiming(
-    { from: 0, to: 1, loop: true },
-    { duration: 2000, easing: Easing.out(Easing.cubic) },
+
+  useDerivedValue(() => {
+    const value = translateX.value;
+
+    percentage.value = withTiming(clampValue(value, TRANSLATE_X_THRESHOLD), {
+      duration: 200,
+      easing: Easing.linear,
+    });
+  });
+
+  // SVG
+  const svg = useSVG(inputSVG);
+  const svgPaint = useSharedValue(Skia.Paint());
+  useAnimatedReaction(
+    () => {
+      return percentage.value;
+    },
+    (opacity) => {
+      svgPaint.value.setAlphaf(opacity);
+    },
   );
 
+  // Border Path
   const borderPath = useMemo(() => {
     const path = Skia.Path.Make();
-
-    // path.addCircle(center.x, center.y, radius);
-    // path.moveTo(size, size - radius);
-
-    path.addArc(
-      // fromCircle(vec(size, size), radius),
-      {
-        x: strokeWidth,
-        y: strokeWidth,
-        width: size - strokeWidth * 2,
-        height: size - strokeWidth * 2,
-      },
-      180,
-      360,
-    );
-
+    path.addArc(fromCircle(vec(x, y), radius), 180, 360);
     return path;
-  }, [strokeWidth]);
+  }, [radius, x, y]);
 
   return (
     <Canvas
-      style={{
-        width: "100%",
-        height: size + strokeWidth * 2,
-      }}
+      style={[
+        {
+          position: "absolute",
+          width: size,
+          height: size,
+          alignItems: "center",
+        },
+        right ? { right: 0 } : { left: 0 },
+      ]}
     >
       <Path
         path={borderPath}
@@ -63,27 +111,35 @@ export const RotatingBorderIcon = (props: PropsWithChildren<Props>) => {
         style="stroke"
         strokeCap="round"
         strokeWidth={strokeWidth}
-        end={progress}
+        end={percentage}
       >
-        <Paint
-          style="stroke"
-          strokeWidth={strokeWidth}
-          strokeCap="round"
-          // color="red"
-        >
+        <Paint style="stroke" strokeWidth={strokeWidth} strokeCap="round">
           <SweepGradient
             colors={["#FF0000", "#00FF00", "#0000FF", "#FF0000"]}
-            c={vec(size / 2, size / 2 - strokeWidth)}
+            c={vec(size / 2, size / 2)}
             start={0}
             end={360}
           />
         </Paint>
       </Path>
-      <Circle
-        color="black"
-        c={vec(size / 2, size / 2)}
-        r={size / 3 - strokeWidth}
-      />
+      {svg && (
+        <Group
+          layer={svgPaint}
+          // transform={fitbox(
+          //   "contain",
+          //   rect(0, 0, 32, 40),
+          //   rect(0, 0, size, size),
+          // )}
+        >
+          <ImageSVG
+            svg={svg}
+            x={size / 3}
+            y={size / 3}
+            width={size / 3}
+            height={size / 3}
+          />
+        </Group>
+      )}
     </Canvas>
   );
 };
